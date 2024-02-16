2f7b7194e79fb34b9f03d4f89ab92dc432d885cd
nurminenlab <lonurmin@central.uh.edu>
2024-02-16 13:05:18 -0600

    Completed spikeGLX preprocessing script

--- process_SGLX.py
+++ process_SGLX.py
@@ -6,33 +6,31 @@ from tkinter import Tk
 from tkinter import filedialog
 import subprocess 
 
-#https://billkarsh.github.io/SpikeGLX/help/syncEdges/Sync_edges/
+CatGT_dir  = ' C:/Users/lonurmin/Desktop/code/CatGT-win'
+TPrime_dir = ' C:/Users/lonurmin/Desktop/code/TPrime-win'
 
 # catGT parameters
 DIR = ' -dir=C:/localDATA/Electrophysiology/MM001-Sansa/2024-02-01'
 RUN = ' -run=RF-mapping-wedge'
 prs = ' -g=0 -t=0 -ap -ni -prb_fld -prb=0'
-xia = ' -xia=0,0,2,2.5,3.5,0'
-xa  = ' -xa=0,0,3,2.5,1,25'
-xa2 = ' -xa=0,0,2,2.5,1,0'
-
+xa2 = ' -xa=0,0,2,2.5,1,0' # trial start
+xia = ' -xia=0,0,2,2.5,3.5,0' # trial stop
+xa  = ' -xa=0,0,4,2.5,1,25' # stim start
 
 # TPrime parameters
 tostream = utils.getFilePath(windowTitle="SYNC tostream file (IMEC0)")
 fromstream = utils.getFilePath(windowTitle="SYNC fromstream file (usually NIDAQ)")
-trialstart = utils.getFilePath(windowTitle="SYNC trial start file (usually xa2)")
-trialstop  = utils.getFilePath(windowTitle="SYNC trial stop file (usually xia2)")
-stimstart  = utils.getFilePath(windowTitle="SYNC trial start file (usually xa4)")
+trialstart = utils.getFilePath(windowTitle="SYNC trial start (usually xa2)")
+trialstop  = utils.getFilePath(windowTitle="SYNC trial stop (usually xia2)")
+stimstart  = utils.getFilePath(windowTitle="SYNC stimulus (usually xa4)")
 
 syncperiod = ' -syncperiod=1.0'
 tostream = ' -tostream='+str(tostream)
 fromstream = ' -fromstream=1,'+str(fromstream)
 trialstart = ' -events=1,'+str(trialstart)+','+str(trialstart)[0:len(str(trialstart))-len(str(trialstart.stem)+'.txt')]+'trialstart.txt'
-trialstop = ' -events=2,'+str(trialstop)+','+str(trialstop)[0:len(str(trialstop))-len(str(trialstop.stem)+'.txt')]+'trialstop.txt'
+trialstop = ' -events=1,'+str(trialstop)+','+str(trialstop)[0:len(str(trialstop))-len(str(trialstop.stem)+'.txt')]+'trialstop.txt'
+stimstart = ' -events=1,'+str(stimstart)+','+str(stimstart)[0:len(str(stimstart))-len(str(stimstart.stem)+'.txt')]+'stimstart.txt'
 
-#' -tostream=C:/localDATA/Electrophysiology/MM001-Sansa/2024-02-01/RF-mapping-wedge_g1/RF-mapping-wedge_g1_imec0/RF-mapping-wedge_g1_tcat.imec0.ap.xd_384_6_500.txt'
-
-CatGT_dir = ' C:/Users/lonurmin/Desktop/code/CatGT-win'
 subprocess.run('cd '+CatGT_dir, shell=True)
 subprocess.run('CatGT'+DIR+RUN+prs+xia+xa+xa2, shell=True)
 
@@ -44,6 +42,9 @@ sRate = readSGLX.SampRate(meta)
 spike_times_smp = np.load(spikesFullPath)
 spike_times_sec = np.around(np.divide(spike_times_smp,sRate,dtype=float),decimals=6)
 np.save(spikesFullPath.with_stem('spike_times_sec'),spike_times_sec)
+
+subprocess.run('cd '+TPrime_dir, shell=True)
+subprocess.run('TPrime'+syncperiod+tostream+fromstream+trialstart+trialstop+stimstart, shell=True)
 
 # run TPrime align spikes to trials
 """ subprocess.run("TPrime -syncperiod=1.0 -tostream=C:\localDATA\Electrophysiology\MM001-Sansa\2024-02-01\RF-mapping-wedge_g1\RF-mapping-wedge_g1_imec0\RF-mapping-wedge_g1_tcat.imec0.ap.xd_384_6_500.txt \
